fcea22392f9d8b2ba9574784ec065a15316a2dff
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-06 22:30:49 -0300

    Adição dos arquivos relativos aos tipos de dados

---
+++ 3_tipos_de_dados/3-2_exercicio_3/exercicio3.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 5;
+echo 50;
+echo 500;
+echo 5000;
---
+++ 3_tipos_de_dados/3-3_checando_inteiro/checando_inteiro.php
@@ -0,0 +1,19 @@
+<?php
+
+if(is_int(5)) {
+  echo "É um número inteiro";
+  echo "<br>";
+} else {
+  echo "Não é um número inteiro";
+  echo "<br>";
+}
+
+$letra = "A";
+
+if(is_int($letra)) {
+  echo "É um número inteiro";
+  echo "<br>";
+} else {
+  echo "Não é um número inteiro";
+  echo "<br>";
+}
